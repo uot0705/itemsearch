8a8eb978bbd2f147f8f2df50289cbe1455e0c3ef
uot0705 <uot2345906abc@gmail.com>
2021-09-30 11:42:55 +0900

    検索バー変更

--- resources/views/layouts/new-master.blade.php
+++ resources/views/layouts/new-master.blade.php
@@ -17,43 +17,10 @@
           <!-- Navigation-->
           <nav class="navbar navbar-expand-lg navbar-light bg-white">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="/">Item Search</a>
-
-                <form class="d-flex">
-                    @if (Route::has('login'))
-                        <div class="hidden fixed top-0 right-0 px-6 py-4 d-block d-md-none">
-                          @auth    <!--ログイン中の場合-->
-                            <a class="dropdown-item" href="{{ route('my.page')}}">マイページ</a>
-                            <a class="dropdown-item" href="{{ route('item.create')}}">アイテム投稿</a>
-                          @else     <!--未ログインの場合-->
-                            <a class="dropdown-item" href="{{ route('login') }}" class="text-sm text-gray-700 underline">ログイン</a>
-                            @if (Route::has('register'))
-                              <a class="dropdown-item" href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">アカウント作成</a>
-                            @endif
-                          @endif
-                        </div>
-                    @endif
-                        
-                    </form>
-                       </div>
-                    <div class="container px-4 px-lg-5">       
-                 <!-- 検索-->
-                    <form class="mb-2 mt-4 text-center" method="GET" action="{{ route('item.search') }}">
-                        <input class="form-control my-2 mr-5" type="search" placeholder="アイテム名を入力" name="search" value="@if (isset($search)) {{ $search }} @endif">
-                        <div class="d-flex justify-content-center">
-                            <button class="btn btn-info my-2" type="submit">検索</button>
-                            <button class="btn btn-secondary my-2 ml-3">
-                                <a href="{{ route('item.search') }}" class="text-decoration-none text-white">
-                                    クリア
-                                </a>
-                            </button>
-                        </div>
-                    </form>
-
-            <!-- 検索終了-->
+                <a class="navbar-brand" href="/">Item Search</a>     
                     <form class="d-flex">
                     @if (Route::has('login'))
-                        <div class="hidden fixed top-0 right-0 px-6 py-4 d-none d-md-block">
+                        <div class="hidden fixed top-0 right-0 px-6 py-4 ">
                           @auth    <!--ログイン中の場合-->
                             <a class="dropdown-item" href="{{ route('my.page')}}">マイページ</a>
                             <a class="dropdown-item" href="{{ route('item.create')}}">アイテム投稿</a>
